0afda07390e02dc180bc414e11cd95c1f0ca3cc5
namanipradeep <56963276+namanipradeep@users.noreply.github.com>
2020-01-21 11:51:26 +0530

    Add files via upload

---
+++ PythonPgm.py
@@ -0,0 +1,21 @@
+name= "Python program"
+
+print(name)
+print(name[0])
+print(name[0:4])
+
+print(name[:10])
+print(name[::]) #total string
+print(name[:]) #total string
+
+print(name[0:16:2]) #o to 15 index pf every 2 character
+
+print(name[-4:-1])
+
+
+print(name[-1])
+
+print(name[::-1]) # reverese a string
+
+
+print(name[::4]) # display every 4 th charactor
